d838e1470be852c13f80a724c8fa2f9feaf22caa
João Pedro <joaoplay16@gmail.com>
2022-04-23 19:39:13 -0300

    Screenshots added

--- app/src/main/java/com/playlab/marvelcomicsinfo/screens/common/ListComic.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/screens/common/ListComic.kt
@@ -1,19 +1,15 @@
 package com.playlab.marvelcomicsinfo.screens.common
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.items
-import com.playlab.marvelcomicsinfo.R
 import com.playlab.marvelcomicsinfo.model.Comic
 import com.playlab.marvelcomicsinfo.screens.ComicCard
-import com.playlab.marvelcomicsinfo.screens.Copyright
 
 @Composable
 fun ListComic (
@@ -21,7 +17,6 @@ fun ListComic (
     modifier: Modifier,
     onComicClick: (Comic) -> Unit,
 ) {
-
     LazyColumn(modifier) {
         items(items = items, key = { it.id }) { comic ->
             comic?.let {
--- app/src/main/java/com/playlab/marvelcomicsinfo/screens/search/SearchScreen.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/screens/search/SearchScreen.kt
@@ -3,7 +3,6 @@ package com.playlab.marvelcomicsinfo.screens.search
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.paging.ExperimentalPagingApi
