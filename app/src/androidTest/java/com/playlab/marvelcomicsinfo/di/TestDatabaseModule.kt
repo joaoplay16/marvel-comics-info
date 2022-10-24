3c3184df8e16510261affea93e730ba5053741f5
João Pedro <joaoplay16@gmail.com>
2022-10-24 17:33:31 -0300

    add test database module

---
+++ app/src/androidTest/java/com/playlab/marvelcomicsinfo/di/TestDatabaseModule.kt
@@ -0,0 +1,55 @@
+package com.playlab.marvelcomicsinfo.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import androidx.room.Room
+import com.playlab.marvelcomicsinfo.data.local.MarvelDatabase
+import com.playlab.marvelcomicsinfo.data.preferences.PreferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestScope
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object TestDatabaseModule {
+
+    @Named("test_db")
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ): MarvelDatabase =
+        Room.inMemoryDatabaseBuilder(
+            context,
+            MarvelDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Named("test_ds")
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext appContext: Context): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            scope = TestScope(),
+            produceFile = {
+                appContext.preferencesDataStoreFile("test_settings")
+            }
+        )
+
+    @Named("test_pds")
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(
+        @Named("test_ds") dataStore: DataStore<Preferences>
+    ): PreferencesDataStore = PreferencesDataStore(dataStore)
+
+}
