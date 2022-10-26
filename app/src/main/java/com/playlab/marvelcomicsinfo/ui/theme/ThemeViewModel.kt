e3ce9af6c6f861ac62da1a765bdbecdb629cac4d
João Pedro <joaoplay16@gmail.com>
2022-10-25 21:08:25 -0300

    refactor to use the theme repository

--- app/src/main/java/com/playlab/marvelcomicsinfo/ui/theme/ThemeViewModel.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/ui/theme/ThemeViewModel.kt
@@ -2,22 +2,25 @@ package com.playlab.marvelcomicsinfo.ui.theme
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.playlab.marvelcomicsinfo.data.preferences.PreferencesDataStore
+import com.playlab.marvelcomicsinfo.repository.ThemeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ThemeViewModel @Inject constructor(
-    private val dataStore: PreferencesDataStore
+    private val themeRepository: ThemeRepository
 ): ViewModel(){
 
-    val isDarkTheme get() = dataStore.isDarkTheme
+    val isDarkTheme get() = themeRepository.isDarkTheme()
 
     fun switchTheme(storedThemeValue: Boolean?, isSystemInDarkTheme: Boolean){
         viewModelScope.launch {
-            if (storedThemeValue == null) dataStore.setDarkTheme(!isSystemInDarkTheme)
-            else dataStore.setDarkTheme(!storedThemeValue)
+            if (storedThemeValue == null) {
+                themeRepository.setIsDarkTheme(!isSystemInDarkTheme)
+            } else {
+                themeRepository.setIsDarkTheme(!storedThemeValue)
+             }
         }
     }
 }
