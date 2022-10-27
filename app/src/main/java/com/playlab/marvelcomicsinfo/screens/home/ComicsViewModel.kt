b47e6aa0530a5622be57ef642e759099bd574010
João Pedro <joaoplay16@gmail.com>
2022-10-27 20:50:07 -0300

    remove datastore dependency

--- app/src/main/java/com/playlab/marvelcomicsinfo/screens/home/ComicsViewModel.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/screens/home/ComicsViewModel.kt
@@ -2,7 +2,6 @@ package com.playlab.marvelcomicsinfo.screens.home
 
 import androidx.lifecycle.ViewModel
 import androidx.paging.ExperimentalPagingApi
-import com.playlab.marvelcomicsinfo.data.preferences.PreferencesDataStore
 import com.playlab.marvelcomicsinfo.repository.ComicRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,10 +9,7 @@ import javax.inject.Inject
 @ExperimentalPagingApi
 @HiltViewModel
 class ComicsViewModel @Inject constructor(
-    repository: ComicRepository,
-    dataStore: PreferencesDataStore
+    repository: ComicRepository
 ): ViewModel() {
-    val copyright = dataStore.getCopyright
-
     val dbComics  = repository.getAllComics()
 }
