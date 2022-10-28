93e9aa9764861ad0e334b4133365ba52e38d69b1
João Pedro <joaoplay16@gmail.com>
2022-10-28 20:11:34 -0300

    implement equals and hashCode

--- app/src/main/java/com/playlab/marvelcomicsinfo/model/Comic.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/model/Comic.kt
@@ -21,4 +21,19 @@ class Comic (
     override fun toString(): String {
         return "ID $id - $title"
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Comic
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
 }
