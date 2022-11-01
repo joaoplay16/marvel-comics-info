d62892ccae824f4b0ee52d53e940aa890aae72b0
João Pedro <joaoplay16@gmail.com>
2022-10-31 21:54:08 -0300

    setup paging source and api

--- app/src/test/java/com/playlab/marvelcomicsinfo/data/paging/TestSearchPagingSource.kt
+++ app/src/test/java/com/playlab/marvelcomicsinfo/data/paging/TestSearchPagingSource.kt
@@ -3,9 +3,19 @@ package com.playlab.marvelcomicsinfo.data.paging
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.playlab.marvelcomicsinfo.MainCoroutineRule
 import com.playlab.marvelcomicsinfo.data.remote.ApiInterface
+import com.playlab.marvelcomicsinfo.model.Comic
+import com.playlab.marvelcomicsinfo.model.ComicResponse
+import com.playlab.marvelcomicsinfo.model.Data
+import com.playlab.marvelcomicsinfo.model.Thumbnail
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
 import org.junit.Rule
+import org.mockito.BDDMockito.*
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
+
+@OptIn(ExperimentalCoroutinesApi::class)
 class TestSearchPagingSource {
 
     @get:Rule
@@ -15,9 +25,36 @@ class TestSearchPagingSource {
     @get:Rule
     var mainCoroutineRule = MainCoroutineRule()
 
-
+    @Mock
     lateinit var api: ApiInterface
 
-    lateinit var searchPagingSource: SearchPagingSource
+    private lateinit var searchPagingSource: SearchPagingSource
 
+    companion object {
+        val searchResponse = ComicResponse(
+            copyright = "",
+            data = Data(
+                limit =1,
+                results = listOf(
+                    Comic(id = "id",
+                        "titulo",
+                        Thumbnail("path", ".jpg"),
+                        null, null))
+            )
+        )
+
+        val nextSearchResponse = ComicResponse(
+            copyright = "",
+            data = Data(
+                limit =1,
+                results = listOf()
+            )
+        )
+    }
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        searchPagingSource = SearchPagingSource(api, "")
+    }
 }
