12e5500e176969ccac93cd2bce3cdf12c0f91a38
João Pedro <joaoplay16@gmail.com>
2022-11-04 22:36:49 -0300

    test search view model test search with empty string

---
+++ app/src/test/java/com/playlab/marvelcomicsinfo/ui/screens/search/TestSearchViewModel.kt
@@ -0,0 +1,58 @@
+package com.playlab.marvelcomicsinfo.ui.screens.search
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.PagingData
+import com.google.common.truth.Truth.assertThat
+import com.playlab.marvelcomicsinfo.MainCoroutineRule
+import com.playlab.marvelcomicsinfo.model.Comic
+import com.playlab.marvelcomicsinfo.repository.FakeComicRepository
+import com.playlab.marvelcomicsinfo.screens.search.SearchViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.test.*
+import org.junit.After
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
+
+@RunWith(MockitoJUnitRunner::class)
+@OptIn(
+    ExperimentalPagingApi::class,
+    ExperimentalCoroutinesApi::class)
+class TestSearchViewModel {
+
+    @get:Rule
+    var instantTaskExecutorRule =  InstantTaskExecutorRule()
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @Mock
+    lateinit var fakeComicRepository: FakeComicRepository
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `search comic with empty query string, returns valid empty data`() = runTest {
+
+        val flowPagingData = MutableStateFlow<PagingData<Comic>>(PagingData.empty())
+
+        given(fakeComicRepository.searchComics(any())).willReturn(flowPagingData)
+
+        val searchViewModel = SearchViewModel(fakeComicRepository)
+        searchViewModel.searchComics("")
+
+        val result = searchViewModel.searchedComics.firstOrNull()
+        assertThat(result).isNotNull()
+    }
+}
