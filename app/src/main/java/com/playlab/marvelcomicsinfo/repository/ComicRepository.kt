6d61deab2764230b5e8186fffef0b0dad3c47996
João Pedro <joaoplay16@gmail.com>
2022-11-07 18:41:43 -0300

    remove unnused imports

--- app/src/main/java/com/playlab/marvelcomicsinfo/repository/ComicRepository.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/repository/ComicRepository.kt
@@ -1,18 +1,9 @@
 package com.playlab.marvelcomicsinfo.repository
 
 import androidx.paging.ExperimentalPagingApi
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.playlab.marvelcomicsinfo.data.local.MarvelDatabase
-import com.playlab.marvelcomicsinfo.data.paging.MarvelRemoteMediator
-import com.playlab.marvelcomicsinfo.data.paging.SearchPagingSource
-import com.playlab.marvelcomicsinfo.data.preferences.PreferencesDataStore
-import com.playlab.marvelcomicsinfo.data.remote.ApiInterface
 import com.playlab.marvelcomicsinfo.model.Comic
-import com.playlab.marvelcomicsinfo.util.Constants.ITEMS_PER_PAGE
 import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
 
 @ExperimentalPagingApi
 interface ComicRepository  {
