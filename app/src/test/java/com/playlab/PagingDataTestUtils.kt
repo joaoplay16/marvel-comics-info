ba2b74c6eff1a887b06b47f150bb8edcec562121
João Pedro <joaoplay16@gmail.com>
2022-11-08 10:47:41 -0300

    improve helper transform function filter by title

--- app/src/test/java/com/playlab/PagingDataTestUtils.kt
+++ app/src/test/java/com/playlab/PagingDataTestUtils.kt
@@ -1,36 +1,33 @@
 package com.playlab
 
 import androidx.paging.PagingData
-import androidx.paging.map
+import androidx.paging.filter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListUpdateCallback
 import com.playlab.marvelcomicsinfo.model.Comic
 
 class PagingDataTestUtils {
     companion object {
-        fun PagingData<Comic>.myHelperTransformFunction(): List<Comic> {
-            val result = mutableListOf<Comic>()
-            this.map { item ->
-                    result.add(item)
-                }
-            return result
-            }
+        fun PagingData<Comic>.myHelperTransformFunction(title: String? = ""): PagingData<Comic> {
+            return if (title.isNullOrEmpty()) this
+            else this.filter { item -> item.title == title }
+        }
+    }
+
+    class NoopListCallback : ListUpdateCallback {
+        override fun onChanged(position: Int, count: Int, payload: Any?) {}
+        override fun onMoved(fromPosition: Int, toPosition: Int) {}
+        override fun onInserted(position: Int, count: Int) {}
+        override fun onRemoved(position: Int, count: Int) {}
+    }
+
+    class MyDiffCallback : DiffUtil.ItemCallback<Comic>() {
+        override fun areItemsTheSame(oldItem: Comic, newItem: Comic): Boolean {
+            return oldItem == newItem
         }
 
-        class NoopListCallback : ListUpdateCallback {
-            override fun onChanged(position: Int, count: Int, payload: Any?) {}
-            override fun onMoved(fromPosition: Int, toPosition: Int) {}
-            override fun onInserted(position: Int, count: Int) {}
-            override fun onRemoved(position: Int, count: Int) {}
+        override fun areContentsTheSame(oldItem: Comic, newItem: Comic): Boolean {
+            return oldItem.id == newItem.id
         }
-
-        class MyDiffCallback : DiffUtil.ItemCallback<Comic>() {
-            override fun areItemsTheSame(oldItem: Comic, newItem: Comic): Boolean {
-                return oldItem == newItem
-            }
-
-            override fun areContentsTheSame(oldItem: Comic, newItem: Comic): Boolean {
-                return oldItem.id == newItem.id
-            }
-        }
+    }
 }
