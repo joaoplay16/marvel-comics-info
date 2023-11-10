689381a95d420aba172cfa6cf90b7218bff0ab20
João Pedro <joaoplay16@gmail.com>
2023-11-09 22:19:36 -0300

    Update `ListComic` to work with the new version of compose-paging
    "androidx.paging:paging-compose:3.2.1"

--- app/src/main/java/com/playlab/marvelcomicsinfo/screens/common/ListComic.kt
+++ app/src/main/java/com/playlab/marvelcomicsinfo/screens/common/ListComic.kt
@@ -7,29 +7,33 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
-import androidx.paging.compose.items
+import com.playlab.marvelcomicsinfo.ext.items
 import com.playlab.marvelcomicsinfo.model.Comic
 import com.playlab.marvelcomicsinfo.screens.components.ComicCard
 
+
 @Composable
-fun ListComic (
+fun ListComic(
     items: LazyPagingItems<Comic>,
     modifier: Modifier,
     onComicClick: (Comic) -> Unit,
 ) {
     LazyColumn(modifier) {
-        items(items = items, key = { it.id }) { comic ->
-            comic?.let {
-                ComicCard(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 20.dp, vertical = 6.dp),
-                    title = comic.title,
-                    description = comic.description,
-                    imageUrl = "${comic.thumbnail.path}.${comic.thumbnail.extension}",
-                    onComicClick = { onComicClick(comic) }
-                )
-            }
+        items(
+            items = items,
+            key = { it.id }) { comic ->
+            ComicCard(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        horizontal = 20.dp,
+                        vertical = 6.dp
+                    ),
+                title = comic.title,
+                description = comic.description,
+                imageUrl = "${comic.thumbnail.path}.${comic.thumbnail.extension}",
+                onComicClick = { onComicClick(comic) }
+            )
         }
     }
 }
